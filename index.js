6ccb215e4a26ab47551e59cf6e47c44b6788b920
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-10-01 18:08:45 +0100

    move js to external file index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+  const bikes = ["My Bike", "Jo\'s Bike", "Milo\'s Bike", "Ruth\'s Bike"];
+
+  const mainView = document.querySelector('#primaryView');
+  mainView.innerHTML = `
+    <h2>Text</h2>
+  `;
